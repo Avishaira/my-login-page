6a3610e08d584470c4aefea6dbac39f49831aec2
RATNER <Avishai.RATNER@FromEarthToLife.net>
2024-06-28 08:43:11 +0300

    Added navbar and made pages responsive

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const navbar = document.getElementById('navbar');
-
-    // Fetch the navbar.html content and insert it into the #navbar div
-    fetch('navbar.html')
+    const navbarPlaceholder = document.getElementById("navbar-placeholder");
+    fetch("navbar.html")
         .then(response => response.text())
         .then(data => {
-            navbar.innerHTML = data;
-
-            // Add scroll event listener after loading navbar content
-            const navbarElement = document.querySelector('.navbar');
-            window.addEventListener('scroll', function() {
-                if (window.scrollY > 50) {
-                    navbarElement.classList.add('scrolled');
-                } else {
-                    navbarElement.classList.remove('scrolled');
-                }
-            });
+            navbarPlaceholder.innerHTML = data;
         });
 });
